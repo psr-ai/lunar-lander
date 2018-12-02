e8a0efff5e322622b838715a4b2aeb9b9c584559
psr <prabhjotsinghrai1@gmail.com>
2018-12-02 15:29:08 +0530

    Feat: implement parametric change to just viewing an existing weight

--- implementation/ddqn/run.py
+++ implementation/ddqn/run.py
@@ -4,27 +4,21 @@ from collections import deque
 from environment import Environment
 from agent import Agent
 
-viewOnly = False
+view_only = True
+output_dir = ''
+initial_weights = './ddqn/model_output_ddqn/weights_2000.hdf5'
+episodes = 10000
 
 PROBLEM = 'LunarLander-v2'
-env = Environment(PROBLEM)
+env = Environment(PROBLEM, should_learn=not view_only, should_render=True)
 
 np.set_printoptions(precision=2)
 
-agent = Agent(env.number_of_states(), env.number_of_actions())
-output_dir = 'model_output/'
-if not os.path.exists(output_dir):
+agent = Agent(env.number_of_states(), env.number_of_actions(), initial_weights=initial_weights, only_exploitation=view_only)
+
+if output_dir and not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-
-# Set to true to use saved model
-
-if viewOnly:
-    agent.model.load_weights('./weights/trained_agent.h5')
-    episodes = 100
-    agent.epsilon = 0
-else:
-    episodes = 10000
 
 # Cumulative reward
 reward_avg = deque(maxlen=100)
@@ -35,9 +29,10 @@ for e in range(episodes):
     average_score = np.average(reward_avg)
     training_stats = 'episode: ', e, ' score: ', '%.2f' % episode_reward, ' avg_score: ', '%.2f' % average_score, ' frames: ', number_of_frames, ' epsilon: ', '%.2f' % agent.epsilon
     print(training_stats)
-    with open(output_dir + 'trained_agent.txt', 'a') as f:
-        f.write(str([e, episode_reward, average_score, number_of_frames, agent.epsilon]) + '\n')
-    if e % 50 == 0:
-        agent.brain.save_weights(output_dir + "weights_" + '{:04d}'.format(e) + ".hdf5", True)
+    if output_dir:
+        with open(output_dir + 'trained_agent.txt', 'a') as f:
+            f.write(str([e, episode_reward, average_score, number_of_frames, agent.epsilon]) + '\n')
+        if e % 50 == 0:
+            agent.brain.save_weights(output_dir + "weights_" + '{:04d}'.format(e) + ".hdf5", True)
 
 env.close()
